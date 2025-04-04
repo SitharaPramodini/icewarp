bb5589eb81bfd5f9d5e8583c73a267314ed5f867
SitharaPramodini <it21306754@my.sliit.lk>
2025-04-04 10:42:18 +0530

    bg hidden

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,12 @@ function Header() {
     <nav className="header fixed rounded-b-2xl w-[101%] h-[55rem]">
       <div className="flex flex-wrap items-center justify-between mx-auto">
         {/* <img src="/home.jpg" className="mx-auto mt-4" alt="Flowbite Logo" /> */}
-        <img src="/icewarp.jpg" className="md:hidden mx-auto w-full" alt="Logo" />
-        <img src="/icewarp1.png" className="mx-auto w-full" alt="Logo" />
+{/* Show on mobile only */}
+<img src="/icewarp.jpg" className="block md:hidden mx-auto w-full" alt="Logo" />
+
+{/* Show on md and larger only */}
+<img src="/icewarp1.png" className="hidden md:block mx-auto w-full" alt="Logo" />
+
         {/* <img src="/icewarpCircle.jpg" className="mx-auto " alt="Flowbite Logo" /> */}
 
         {/* <h1 className="absolute mx-auto mb-4 text-4xl font-extrabold leading-none tracking-tight text-[#fc5e28] md:text-5xl lg:text-6xl">
